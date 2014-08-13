db7edb38094b600b6700f1f1478b816b67679e3f
louis.yu <yf_hello@163.com>
2014-08-13 14:42:54 +0800

    change title

--- modules/chartdlg.py
+++ modules/chartdlg.py
@@ -14,6 +14,8 @@ class ChartDlg(QDialog):
         self.setWindowFlags(Qt.Tool|Qt.MSWindowsFixedSizeDialogHint)
         self.resize(550, 300)
         
+        self.setWindowTitle('Chart')
+
         self.chart = QLabel('')
 
         grid = QGridLayout()
@@ -25,7 +27,6 @@ class ChartDlg(QDialog):
         self.setLayout(grid)
 
     def updateChart(self, code):
-        self.setWindowTitle(code)
         imageUrl = stock.getStockChartUrl(code)
         url = QUrl(imageUrl)
         m_netwManager = QNetworkAccessManager(self)
