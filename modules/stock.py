f2775e106b18f601150b439840dd699a487f2c13
louis.yu <yf_hello@163.com>
2014-08-26 14:35:32 +0800

    add forex support

--- modules/stock.py
+++ modules/stock.py
@@ -28,6 +28,8 @@ def getStockHq(code_list):
             info = hk_info(dataArr)
         elif(code[:6] == 'CFF_IF'):
             info = if_info(dataArr)
+        elif(code[:6] == code and re.match('^[A-Z]+$',code)):
+            info = forex_info(dataArr)            
         else :
             info = qihuo_info(dataArr)
 
@@ -106,13 +108,35 @@ def if_info(dataArr):
     info = {}
     return info
 
+#外汇信息
+def forex_info(dataArr):
+    info = {}
+    info['name'] = dataArr[9]
+    info['open'] = float(dataArr[5])
+    info['close_yesterday'] = float(dataArr[5])
+    info['price'] = float(dataArr[1])
+    if(info['price']==0):
+        info['price'] = info['close_yesterday']   
+
+    info['close'] = info['price']
+    info['high'] = float(dataArr[6])
+    info['low'] = float(dataArr[7]) 
+    if info['close_yesterday'] > 0:
+        info['price_change'] = info['price']-info['close_yesterday']
+        info['price_change_percent'] = (info['price']-info['close_yesterday'])*100/info['close_yesterday']  
+
+    info['time'] = dataArr[0]  
+    return info
+
 
 def getStockChartUrl(code):
     if(code[:2] == 'sh' or code[:2] == 'sz'):
         imageUrl = 'http://image.sinajs.cn/newchart/min/n/{}.gif?{}'.format(code, random.randint(1,1000000))
     elif(code[:2] == 'hk'):
-        imageUrl = 'http://image.sinajs.cn/newchart/hk_stock/min/{}.gif?{}'.format(code[2:], random.randint(1,1000000))
+        imageUrl = 'http://image.sinajs.cn/newchart/v5/hk_stock/min/{}.gif?{}'.format(code[2:], random.randint(1,1000000))
+    elif(code[:6] == code and re.match('^[A-Z]+$',code)):
+        imageUrl = 'http://image.sinajs.cn/newchart/v5/forex/min/{}.gif?{}'.format(code, random.randint(1,1000000))
     else:
-        imageUrl = 'http://image.sinajs.cn/newchart/futures/min/{}.gif?{}'.format(code, random.randint(1,1000000))
+        imageUrl = 'http://image.sinajs.cn/newchart/v5/futures/min/{}.gif?{}'.format(code, random.randint(1,1000000))
 
     return imageUrl
